3e7b970a2cd12c21193c2d2384f2106616716b0d
Aleksandar Milinkovic <milinkovicaleks@gmail.com>
2024-02-16 11:10:39 +0100

    added atm module

--- tests/test_atm.py
+++ tests/test_atm.py
@@ -1,4 +1,6 @@
+from modules import atm
+
 def describe_atm():
     def should_validate_true():
         """🧪 expect the validation to be true"""
-        assert True == True
+        assert atm.validate() == True
